31a097deb5ed4065d941b6a0490c15c88def1da2
William Moore <caranmegil@gmail.com>
2022-01-08 04:35:40 -0600

    Add links to return to collections pages for Dice Collections and Force Builder.

--- src/components/DiceBrowser.vue
+++ src/components/DiceBrowser.vue
@@ -29,6 +29,10 @@
                 </div>
               </span>
 
+              <div class="anchor-element">
+                <a @click="returnToModifier">Return to My Collection</a>
+              </div>
+
               <div class="table-header">
                   <div class="column-header die-id" @click="changeNameDirection">Name <span v-if="sortColumn != 0" class="material-icons material-icons-outlined">unfold_more</span><span v-if="sortColumn == 0 && sortDirection == -1" class="sort-icon material-icons material-icons-outlined">expand_less</span><span v-if="sortColumn == 0 && sortDirection == 1" class="sort-icon material-icons material-icons-outlined">expand_more</span></div>
                   <div class="column-header size" @click="changeSizeDirection">Size  <span v-if="sortColumn != 1" class="material-icons material-icons-outlined">unfold_more</span><span v-if="sortColumn == 1 && sortDirection == -1" class="sort-icon material-icons material-icons-outlined">expand_less</span><span v-if="sortColumn == 1 && sortDirection == 1" class="sort-icon material-icons material-icons-outlined">expand_more</span></div>
@@ -293,6 +297,9 @@ export default {
             this.filteredDice = this.applyFiltersAndSort();
             this.isLoading=false;
         },
+        returnToModifier: function() {
+          this.$router.push('/my-collection');
+        },
     },
 };
 </script>
@@ -305,6 +312,12 @@ export default {
   #dice .header h1 {
     align-self: center;
     justify-self: center;    
+  }
+  #dice .header .anchor-element {
+    align-self: center;
+    justify-self: center;
+    display: grid;
+    grid-auto-flow: column;
   }
 
   #dice .header .element {
