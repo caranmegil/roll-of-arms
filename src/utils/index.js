26ec655b1ef403ad5424adbd777d7c7ef9d0af30
William Moore <caranmegil@gmail.com>
2022-03-07 18:20:01 -0600

    Add ability to convert older editions to new edition.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,12 @@
 export const sleep = (timeout) => new Promise(resolve => setTimeout(resolve, timeout));
+
+export const convertEditionForDie = die => {
+    let newEdition = die.edition;
+    if (die.edition === 'SFR Terrain' && die.rarity === 'Advanced Terrain') {
+        newEdition = 'SFR Advanced Terrain Gold Ink';
+    } else if (die.name.includes('Deadland')) {
+        newEdition = 'SFR Deadlands Green Ink';
+    }
+
+    return newEdition;
+};
