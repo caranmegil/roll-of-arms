64ee6c1f682553feedba10226cd471dec125f31a
William Moore <caranmegil@noreply.codeberg.org>
2022-06-07 13:50:49 -0500

    Update for Terraform

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "docker_image" "rollofarms" {
 }
 
 resource "docker_container" "rollofarms" {
-  image = docker_image.rollofarms.latest
+  image = docker_image.rollofarms.name
   name  = "rollofarms"
   ports {
     internal = 8080
